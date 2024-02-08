a19925dc60d5c17b7ecefdf641b7538e5dd8e55a
4512 Otter Chaos <otterchaos4512@gmail.com>
2024-02-07 16:05:13 -0800

    Intake

--- src/main/java/frc/robot/Subsystems/Intake/Intake.java
+++ src/main/java/frc/robot/Subsystems/Intake/Intake.java
@@ -2,16 +2,70 @@ package frc.robot.subsystems.Intake;
 
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
-import frc.robot.util.OCSparkMax;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class Intake {
-    private OCSparkMax intakeMotor = new OCSparkMax(1, MotorType.kBrushless);
+import frc.robot.util.OCSparkMax;
+import frc.robot.util.OCSparkMax.OCRelativeEncoder;
+
+public class Intake extends SubsystemBase{
+    private OCSparkMax motor = new OCSparkMax(1, MotorType.kBrushless);
+
+    OCRelativeEncoder encoder = motor.getEncoder();
+    
+    private SimpleMotorFeedforward ff = new SimpleMotorFeedforward(0.25, 0.005);
+    private PIDController pid = new PIDController(0.005, 0, 0);
+    boolean isManual = true;
     
     public Intake(){
-
+        // intakeMotor.setCANTimeout(100);
+        // OCConfig.configMotors(kMotorStallLimit, kMotorStallLimit, kRampRate, leftMotor, rightMotor);
+        // OCConfig.setStatusNormal(leftMotor, rightMotor);
+        // OCConfig.setIdleMode(IdleMode.kBrake, leftMotor, rightMotor);
+        // intakeMotor.setCANTimeout(0);
+		// OCConfig.saveConfig(leftMotor, rightMotor);
     }
 
-    public void setVoltage{
-        
+    // @Override
+    public void periodic() {
+        if(!isManual) {
+            motor.setVoltage(ff.calculate(pid.getSetpoint())+pid.calculate(encoder.getVelocity()));
+        }
+    }
+
+    public void setVoltage(double voltage){
+        isManual=true;
+        motor.setVoltage(voltage);
+    }
+
+    public void setVelocity(double rpm) {
+        isManual = false;
+        pid.setSetpoint(rpm);
+    }
+
+    public Command setVoltageC(double voltage){
+        return run(()->setVoltage(voltage));
+    }
+
+    public Command setVoltageInC(){
+        return run(()->setVoltage(2));
+    }
+
+    public Command setVoltageOutC(){
+        return run(()->setVoltage(-2));
+    }
+
+    public Command setVelocityC(double velocity){
+        return run(()->setVelocity(velocity));
+    }
+
+    public Command setVelocityInC(){
+        return run(()->setVelocity(30));
+    }
+
+    public Command setVelocityOutC(){
+        return run(()->setVelocity(-30));
     }
 }
--- src/main/java/frc/robot/Subsystems/Intake/IntakeConstants.java
+++ src/main/java/frc/robot/Subsystems/Intake/IntakeConstants.java
@@ -1,5 +1,11 @@
 package frc.robot.subsystems.Intake;
 
 public class IntakeConstants {
-    
+
+    public static final int kMotorStallLimit = 40;
+
+    public static final double kMotorStallDetection = 15;
+
+    public static final double kRampRate = 0.08;
+
 }
