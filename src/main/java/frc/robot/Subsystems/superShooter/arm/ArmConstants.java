57d17a381314696394274ad274e4440cd94f5c9c
4512 Otter Chaos <otterchaos4512@gmail.com>
2024-02-07 16:17:50 -0800

    armconstantsgobrrrr

--- src/main/java/frc/robot/Subsystems/superShooter/arm/ArmConstants.java
+++ src/main/java/frc/robot/Subsystems/superShooter/arm/ArmConstants.java
@@ -3,7 +3,7 @@ package frc.robot.subsystems.superShooter.arm;
 public class ArmConstants {
     public static double kShoulderVoltage = 4;
     public static double kMaxRotationDegrees = 110;
-    public static double kCountsPerRevolution = 2048*(52/18);
+    public static double kCountsPerRevolution = 2048*(52/18)*(5/3);
 
     public static double kP = 1;
     public static double kI = 0;
