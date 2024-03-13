cd1d169b6b18b42ac5e6d5bc2b149cc72bd5b13a
amquake <noleetarrr@gmail.com>
2024-03-13 04:01:45 -0700

    add vision simulation

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -12,6 +12,8 @@ import com.ctre.phoenix6.SignalLogger;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.simulation.BatterySim;
+import edu.wpi.first.wpilibj.simulation.RoboRioSim;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.auto.AutoOptions;
 import frc.robot.subsystems.Superstructure;
@@ -20,19 +22,22 @@ import frc.robot.subsystems.drive.SwerveDrive;
 import frc.robot.subsystems.feeder.Feeder;
 import frc.robot.subsystems.intake.Intake;
 import frc.robot.subsystems.shooter.Shooter;
+import frc.robot.subsystems.vision.Vision;
 import frc.robot.util.OCXboxController;
 
 public class Robot extends TimedRobot {
-    private SwerveDrive drive = new SwerveDrive();
+    private SwerveDrive swerve = new SwerveDrive();
     private Climber climber = new Climber();
     private Intake intake = new Intake();
     private Feeder feeder = new Feeder();
     // private Arm arm = new Arm();
     private Shooter shooter = new Shooter();
 
-    private Superstructure superstructure = new Superstructure(drive, intake, shooter, feeder);
+    private Vision vision = new Vision();
 
-    private AutoOptions autos = new AutoOptions(drive, intake, shooter, feeder, superstructure);
+    private Superstructure superstructure = new Superstructure(swerve, intake, shooter, feeder);
+
+    private AutoOptions autos = new AutoOptions(swerve, intake, shooter, feeder, superstructure);
     
     private OCXboxController driver = new OCXboxController(0);
     private OCXboxController operator = new OCXboxController(1);
@@ -84,20 +89,20 @@ public class Robot extends TimedRobot {
 
     private void configureDriverBinds(OCXboxController controller){
         //TODO: use real max drivespeed meters/second
-        drive.setDefaultCommand(
+        swerve.setDefaultCommand(
             run(()->{
                     int invert=1;
-                    if (drive.flipAutoOrgin()){
+                    if (swerve.flipAutoOrgin()){
                         invert=-1;
                     }
-                    drive.drive(
-                        controller.getForward()*drive.getMaxLinearVelocityMeters()*invert,
-                        controller.getStrafe()*drive.getMaxLinearVelocityMeters()*invert,
-                        controller.getTurn()*drive.getMaxAngularVelocityRadians(),
+                    swerve.drive(
+                        controller.getForward()*swerve.getMaxLinearVelocityMeters()*invert,
+                        controller.getStrafe()*swerve.getMaxLinearVelocityMeters()*invert,
+                        controller.getTurn()*swerve.getMaxAngularVelocityRadians(),
                         true
                     );
                 },
-                drive
+                swerve
             )
         );
     
@@ -132,9 +137,9 @@ public class Robot extends TimedRobot {
         // reset the robot heading to 0
         controller.start()
             .onTrue(runOnce(()->
-                drive.resetOdometry(
+                swerve.resetOdometry(
                     new Pose2d(
-                        drive.getPose().getTranslation(),
+                        swerve.getPose().getTranslation(),
                         new Rotation2d()
                     )
                 )
@@ -160,5 +165,13 @@ public class Robot extends TimedRobot {
     public void simulationInit() {}
     
     @Override
-    public void simulationPeriodic() {}
+    public void simulationPeriodic() {
+        // Update camera simulation
+        // TODO: dont use the pose estimator pose
+        vision.simulationPeriodic(swerve.getPose());
+
+        // Calculate battery voltage sag due to current draw
+        RoboRioSim.setVInVoltage(
+                BatterySim.calculateDefaultBatteryLoadedVoltage(swerve.getCurrentDraw()));
+    }
 }
